63b90f40b4f5e9e69d69d72f851c86841cdc170b
AhmedSa-mir <asamir97@hotmail.com>
2020-04-28 19:56:42 +0200

    Implemented project_list and miniproject_list

--- src/frontend/src/components/MiniProjectList.svelte
+++ src/frontend/src/components/MiniProjectList.svelte
@@ -1,59 +1,16 @@
 <script>
-  import MiniArticles from "./MiniArticles.svelte";
-  import UsersComponent from "./Users.svelte";
+  import MiniProject from "./MiniProject.svelte";
 
-  let res = undefined;
+  let miniProjects = [];
 
-  // list of MiniProjects
 </script>
 
-<!-- Sidebar -->
-<section id="sidebar">
-
-  <!-- Intro -->
-  <section id="intro">
-    <a href="#" class="logo">
-      <img src="images/logo.jpg" alt="" />
-    </a>
-    <header>
-      <h2>Future Imperfect</h2>
-      <p>
-        Another fine responsive site template by
-        <a href="http://html5up.net">HTML5 UP</a>
-      </p>
-    </header>
-  </section>
-
-  {#if res === undefined}
-    <p />
-  {:else}
-    {#await res}
-
-      <p>Loading...</p>
-
-    {:then items}
-      <!-- Mini Posts -->
-      <MiniArticles miniarticles={items.projects} />
-      <!-- users List -->
-      <UsersComponent users={items.users} />
-    {:catch error}
-      {error.message}
-    {/await}
-  {/if}
-
-  <!-- About -->
-  <section class="blurb">
-    <h2>About</h2>
-    <p>
-      Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed
-      mattis nunc id lorem euismod amet placerat. Vivamus porttitor magna enim,
-      ac accumsan tortor cursus at phasellus sed ultricies.
-    </p>
-    <ul class="actions">
+<div class="miniProjectList">
+  <ul>
+    {#each miniProjects as miniProject}
       <li>
-        <a href="#" class="button">Learn More</a>
+        <MiniProject {miniProject} />
       </li>
-    </ul>
-  </section>
-
-</section>
+    {/each}
+  </ul>
+</div>
--- src/frontend/src/components/Project.svelte
+++ src/frontend/src/components/Project.svelte
@@ -1,3 +1,23 @@
 <script>
-  // project card
+  export let project;
 </script>
+
+<div class="project">
+  <header>
+    <div class="title">
+      <h2><a href="">Project Title</a></h2>
+      <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
+    </div>
+    <div class="meta">
+      <time class="published" datetime="2015-11-01">November 1, 2015</time>
+      <a href="#" class="author"><span class="name">Jane Doe</span><img src="images/avatar.jpg" alt="" /></a>
+    </div>
+  </header>
+  <a href="" class="image featured"><img src="" alt="" /></a>
+  <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+  <footer>
+    <ul class="actions">
+      <li><a href="" class="button large">Continue Reading</a></li>
+    </ul>
+  </footer>
+</div>
