4f752ad2f68fe6774f625726757aa34f0c2285c5
hamdy <hamdy.a.farag@gmail.com>
2020-04-29 16:25:20 +0200

    build scripts

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@ npm run build
 echo "copying data into public"
 cp -r public/* ../../public
 echo "DONE"
+echo "\n"
+echo "build for production"
+cd -
+crystal build --release src/walker.cr
+echo "DONE"
---
+++ run.sh
@@ -0,0 +1,1 @@
+KEMAL_ENV=production ./walker
