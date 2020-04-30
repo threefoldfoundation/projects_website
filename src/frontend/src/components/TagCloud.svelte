fcd019acf49896f318484984d4c6e4ba5400864b
samaradel <samar@incubaid.com>
2020-05-01 00:41:23 +0200

    Restyle tags

--- src/frontend/src/components/TagCloud.svelte
+++ src/frontend/src/components/TagCloud.svelte
@@ -1,10 +1,14 @@
 <script>
-export let tags;
-console.log(tags)
-console.log("hey")
+  export let tags;
+  console.log(tags);
+  console.log("hey");
 </script>
-<ul>
-{#each tags as tag}
-    <li><a href="#/projects/tags/{tag}">{tag}</a></li>
-    {/each}
+
+<ul class="stats">
+  <h1>Tags</h1>
+  {#each tags as tag}
+    <li class="my-1">
+      <a href="#/projects/tags/{tag}">{tag}</a>
+    </li>
+  {/each}
 </ul>
