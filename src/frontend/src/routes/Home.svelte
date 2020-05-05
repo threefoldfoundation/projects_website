2d0c5f044c86408c88a51412331647fc558a844a
ehab-hassan <hassane@codescalers.com>
2020-05-05 20:24:56 +0200

    update home text

--- src/frontend/src/routes/Home.svelte
+++ src/frontend/src/routes/Home.svelte
@@ -23,30 +23,32 @@
       <p class="home_tf">
         We are
         <a href="http://threefold.io/" target="_blank">ThreeFold</a>
-        , and have built a new peer-to-peer internet that empowers equality,
-        freedom and sustainability. As we launch the
+        , and we have built a new peer-to-peer internet that empowers equality,
+        autonomy and sustainability. As we launch the
         <a href="https://threefold.io/capacity.html" target="_blank">
           ThreeFold Grid
         </a>
-        and many other experiences on top, the time has come for us to make our
-        world peer-to-peer.
+         and other experiences on top, the time has come for us to make our world peer-to-peer.
       </p>
 
       <p class="home_tf">
         On the 29th of April 2020, ThreeFold started a rolling launch with many
-        exciting announcements for the world. This began with the announcement
-        of our Grid 2.0 upgrade and the availability of the
+        announcements for the world. This began with the announcement of our
+        Grid 2.0 upgrade and the availability of the
         <a href="https://threefold.io/token.html" target="_blank">
           ThreeFold Token
         </a>
-        on Stellar, followed by partnership and product announcements across the
-        following weeks.
+        on Stellar, to be followed by partnership and product announcements over
+        the coming weeks.
+
+
       </p>
       <p class="home_tf">
-        The time has come to start building our peer-to-peer world together, and
-        it goes without saying that a collective and equal approach is needed.
-        Many beautiful projects have joined the Planet First Ecosystem, and
-        together we aim to empower a better future for humanity and our planet.
+      The time has come to start building our peer-to-peer world together, and
+      it goes without saying that a collective and equal approach is needed.
+      Many beautiful projects have joined the Conscious Internet Alliance,
+      and together we aim to empower a better future for humanity and our planet.
+
       </p>
       <p class="home_tf">
         If not
