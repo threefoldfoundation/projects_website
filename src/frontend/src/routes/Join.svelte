fd819470e9ec86437513fd0d1be99c987b11e47b
ehab-hassan <hassane@codescalers.com>
2020-05-05 21:02:48 +0200

    fix

--- src/frontend/src/routes/Join.svelte
+++ src/frontend/src/routes/Join.svelte
@@ -37,28 +37,7 @@
       <span class="image featured">
         <img src="images/pic01.png" alt="" />
       </span>
-      <p class="home_tf">
-        We are
-        <a href="http://threefold.io/" target="_blank">ThreeFold</a>
-        , and have built a new peer-to-peer internet that empowers equality,
-        freedom and sustainability. As we launch the
-        <a href="https://threefold.io/capacity.html" target="_blank">
-          ThreeFold Grid
-        </a>
-        and many other experiences on top, the time has come for us to make our
-        world peer-to-peer.
-      </p>
 
-      <p class="home_tf">
-        On the 29th of April 2020, ThreeFold started a rolling launch with many
-        exciting announcements for the world. This began with the announcement
-        of our Grid 2.0 upgrade and the availability of the
-        <a href="https://threefold.io/token.html" target="_blank">
-          ThreeFold Token
-        </a>
-        on Stellar, followed by partnership and product announcements across the
-        following weeks.
-      </p>
       <p class="home_tf">
         We are not meant to do anything alone. Creating a harmonic and healthy Planet requires people coming together and joining forces with a single vision of doing conscious business.
       </p>
@@ -66,7 +45,7 @@
       Our world needs change! Together we can enable a new global peer-to-peer economy of human and planet centric technologies and solutions guided by universal principles.
       Everything starts with core values. From ego to love, from pyramids to equality, from marketing to transparency.
       </p>
-      
+
       <p class="home_tf">We stand by
         <a href="https://www.freeflownation.org/" target="_blank">FreeFlow Nation's</a>
         vision and manifesto. If you find yourself aligning with this collective vision use the form below let us know more about you and let’s enable a better world together!
@@ -82,7 +61,7 @@
             <label for="about">Tell us about yourself:</label><br>
             <input type="text" id="about" name="about"><br>
 
-            <input type="submit">	
+            <input type="submit">
             </form>
     </article>
   </div>
