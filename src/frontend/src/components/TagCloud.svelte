eb9743ce8c9be317bf7ba088c2115afdafd61009
hamdy <hamdy.a.farag@gmail.com>
2020-05-05 15:52:43 +0200

    fixed tagcloud selection

--- src/frontend/src/components/TagCloud.svelte
+++ src/frontend/src/components/TagCloud.svelte
@@ -5,47 +5,49 @@ export let tags;
   if(window.location.href.includes("users")){
       tags_category = "users"
   }
+  tags = tags.filter(t =>
+      t.href.includes(tags_category)
+  );
 
-  $: selected = "all"
-  
-  function tagSelected(evt){
-    selected = evt.target.innerText.toLowerCase()
-    
+  function getSelected(t){
+
+    var projectsIdx = t.indexOf("#/projects/tags")
+    var peopleIdx = t.indexOf("#/users/tags")
+    if (projectsIdx != -1)
+      return t.substring(projectsIdx+16)
+    else if (peopleIdx != -1)
+      return t.substring(projectsIdx+13)
+    else 
+      return "all"
   }
+
+  $: selected = getSelected(location.href)
+
+  function tagSelected(evt){
+    console.log()
+    selected = evt.target.innerText.toLowerCase()
+  }
+  
 
 </script>
 
 <ul class="stats">
   <h1>Tags</h1>
   {#each tags as tag}
-      {#if tag.href.includes("users") && tags_category == "users"}
       {#if tag.name == selected}
-      <li class="my-1"><a class= "active" on:click={tagSelected} href="{tag.href}">{tag.name}</a></li>
+      <li class="my-1"><a  on:click={tagSelected} class= "active" href="{tag.href}">{tag.name}</a></li>
       {:else}
       <li class="my-1"><a  on:click={tagSelected} href="{tag.href}">{tag.name}</a></li>
       {/if}
-      
-      {:else if tag.href.includes("projects") && tags_category == "projects"}
-        {#if tag.name == selected}
-      <li class="my-1"><a  on:click={tagSelected} class= "active" href="{tag.href}">{tag.name}</a></li>
-        {:else}
-        <li class="my-1"><a  on:click={tagSelected} href="{tag.href}">{tag.name}</a></li>
-        {/if}
-      {/if}
   {/each}
 
-  {#if tags_category == "users"}
-    {#if selected == "all"}
-     <li class="my-1"><a  on:click={tagSelected} class= "active" href="#/users">ALL</a></li>
-     {:else}
-     <li class="my-1"><a  on:click={tagSelected} href="#/users">ALL</li>
-     {/if}
-  {:else}
-  {#if selected == "all"}
+  {#if selected == "all" && tags_category == "projects"}
      <li class="my-1"><a  on:click={tagSelected} class= "active" href="#/projects">ALL</a></li>
-     {:else}
-     <li class="my-1"><a  on:click={tagSelected} href="#/projects">ALL</a></li>
-     {/if}
+  {:else if tags_category == "projects"}
+     <li class="my-1"><a   on:click={tagSelected} href="#/projects">ALL</a></li>
+  {:else if selected == "all" && tags_category == "users"}
+  <li class="my-1"><a  on:click={tagSelected}  class= "active" href="#/users">ALL</a></li>
+  {:else if tags_category == "users"}
+  <li class="my-1"><a  on:click={tagSelected} href="#/users">ALL</a></li>
   {/if}
-
 </ul>
