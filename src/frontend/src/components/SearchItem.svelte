613f9ce2d0c20e2b8c70d0c87964ff7476b8f211
samaradel <samar@incubaid.com>
2020-05-07 14:24:58 +0200

    style search items

--- src/frontend/src/components/SearchItem.svelte
+++ src/frontend/src/components/SearchItem.svelte
@@ -1,20 +1,19 @@
 <script>
   import marked from "marked";
   export let item;
+  console.log(item);
 </script>
 
-<article class="post">
+<article class="post search_result">
   <header>
     <div class="title">
-    {#if item.isUser}
-    <h1>User</h1>
+      {#if item.isUser}
+        <h3>User:</h3>
       {:else}
-      <h1>Company</h1>
+        <h3>Company:</h3>
       {/if}
-      <h2>
-        <a href="#/projects/{item.name}">{item.name}</a>
-      </h2>
-      
+      <a class="text-uppercase" href="#/projects/{item.name}">{item.name}</a>
+
     </div>
   </header>
 </article>
