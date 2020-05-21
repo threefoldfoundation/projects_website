f7eb9cc175048e3b39f80ede60f03c8775d9da59
samaradel <samar@incubaid.com>
2020-05-21 14:56:24 +0200

    Restyle home page

--- src/frontend/src/routes/Home.svelte
+++ src/frontend/src/routes/Home.svelte
@@ -11,64 +11,79 @@
   <div id="main">
 
     <!-- Post -->
-    <article class="container text-dark font-weight-normal">
-      <span class="image featured">
-        <img src="images/green-header2.png" alt="" />
-      </span>
-      <header class="text-center my-4">
-        <div class="title">
-          <h2 class="text-uppercase my-3">conscious internet alliance</h2>
-          <p>
-            A collective and open ecosystem of planet and human centric projects
+    <article class="container font-weight-normal">
+      <div class="row">
+
+        <div class="col-sm-10 offset-sm-1">
+          <span class="image featured">
+            <img src="images/green-header2.png" alt="" />
+          </span>
+          <header class="text-center my-5">
+            <div class="title">
+              <h1 class="display-4 text-uppercase my-3">
+                conscious internet alliance
+              </h1>
+              <p class="font-weight-normal h3">
+                A collective and open ecosystem of planet and human centric
+                projects
+              </p>
+            </div>
+          </header>
+
+          <p class="home_tf">
+            We are
+            <a href="http://threefold.io/" target="_blank">ThreeFold</a>
+            and we have built a bridge to a new peer-to-peer internet. Internet
+            that empowers equality, autonomy and sustainability.
           </p>
+          <p class="home_tf">
+            As we launch the
+            <a href="https://threefold.io/capacity.html" target="_blank">
+              Grid
+            </a>
+            and other experiences on top, the time has come for us to cross over
+            to the decentralized world of peer-to-peer.
+          </p>
+
+          <p class="home_tf">
+            A decentralized peer-to-peer world would be incomplete without
+            <a href="https://threefold.io/token.html" target="_blank">
+              ThreeFold Token
+            </a>
+            - a purpose driven decentralized digital currency serving as a fair
+            and trustworthy medium of exchange.
+          </p>
+
+          <p class="home_tf">
+            A decentralized peer-to-peer world would be impossible without the
+            <a
+              href="https://www.consciousinternet.org/#/projects"
+              target="_blank">
+              Conscious Internet Alliance
+            </a>
+            , an ecosystem of beautifully aligned projects co-creating it.
+          </p>
+
+          <p class="home_tf">
+            With all the underlying elements, together we are co-creating the
+            new peer-to-peer internet, empowering a better future for humanity
+            and our planet.
+          </p>
+          <p class="home_tf text-center">
+            If not
+            <span class="home_span">us</span>
+            , who? If not
+            <span class="home_span">now</span>
+            , when?
+          </p>
+          <div class="text-center link_wrapper my-4">
+            <a href="#/projects" class="button large text-uppercase">
+              see projects
+            </a>
+          </div>
         </div>
-      </header>
-
-      <p class="home_tf">
-        We are
-        <a href="http://threefold.io/" target="_blank">ThreeFold</a>
-        and we have built a bridge to a new peer-to-peer internet. Internet that
-        empowers equality, autonomy and sustainability.
-      </p>
-      <p class="home_tf">
-        As we launch the
-        <a href="https://threefold.io/capacity.html" target="_blank">Grid</a>
-        and other experiences on top, the time has come for us to cross over to
-        the decentralized world of peer-to-peer.
-      </p>
-
-      <p class="home_tf">
-        A decentralized peer-to-peer world would be incomplete without
-        <a href="https://threefold.io/token.html" target="_blank">
-          ThreeFold Token
-        </a>
-        - a purpose driven decentralized digital currency serving as a fair and
-        trustworthy medium of exchange.
-      </p>
-
-      <p class="home_tf">
-        A decentralized peer-to-peer world would be impossible without the
-        <a href="https://www.consciousinternet.org/#/projects" target="_blank">
-          Conscious Internet Alliance
-        </a>
-        , an ecosystem of beautifully aligned projects co-creating it.
-      </p>
-
-      <p class="home_tf">
-        With all the underlying elements, together we are co-creating the new
-        peer-to-peer internet, empowering a better future for humanity and our
-        planet.
-      </p>
-      <p class="home_tf text-center">
-        If not
-        <span class="home_span">us</span>
-        , who? If not
-        <span class="home_span">now</span>
-        , when?
-      </p>
-      <div class="text-center link_wrapper my-4">
-        <a href="#/projects" class="button large text-uppercase">learn more</a>
       </div>
+
     </article>
   </div>
 </div>
