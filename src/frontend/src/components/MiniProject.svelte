4fea79856a8b3e2ae0a3a8decb4fe51d87f69815
samaradel <samar@incubaid.com>
2020-05-26 13:02:07 +0200

    Fix logos

--- src/frontend/src/components/MiniProject.svelte
+++ src/frontend/src/components/MiniProject.svelte
@@ -12,11 +12,14 @@
 
   {#if miniProject.links.logo_path != ''}
     <a href="#/projects/{miniProject.name}" class="image">
-      <img src={miniProject.links.logo_path} alt="" />
+      <img
+        class="img-thumbnail border-0"
+        src={miniProject.links.logo_path}
+        alt="" />
     </a>
   {:else}
     <a href="#/projects/{miniProject.name}" class="image ">
-      <img src="images/pic01.jpg" alt="" />
+      <img class="img-thumbnail border-0" src="images/pic01.jpg" alt="" />
     </a>
   {/if}
 </article>
