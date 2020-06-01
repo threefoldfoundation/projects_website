73ca335331dec2568b1de0a72ae889402dca2aaf
samaradel <samar@incubaid.com>
2020-06-01 12:26:40 +0200

    get old content of home

--- src/frontend/src/routes/Home.svelte
+++ src/frontend/src/routes/Home.svelte
@@ -11,46 +11,60 @@
   <div id="main">
 
     <!-- Post -->
-    <article class="container font-weight-normal text-dark">
+    <article class="container font-weight-normal">
       <div class="row">
         <span class="image featured">
           <img src="images/green-header2.png" alt="" />
         </span>
         <div class="col-sm-10 offset-sm-1">
-          <header class="text-center mx-auto mb-4">
+          <header class="text-center mx-auto mb-5">
             <div class="title">
-              <h3 class="font-weight-normal">Alliance for</h3>
-              <h2 class="text-uppercase font-weight-normal">
-                conscious internet
-              </h2>
-              <p class="font-weight-normal my-3">
-                a collective and open ecosystem of planet and human centric
-                projects forming a new internet
+              <h1 class="text-uppercase my-3">conscious internet alliance</h1>
+              <p class="font-weight-normal h3">
+                A collective and open ecosystem of planet and human centric
+                projects
               </p>
             </div>
           </header>
           <p class="home_tf">
-            <span class="d-block font-weight-bold text-capitalize">
-              our story:
-            </span>
-            It all started with
+            We are
             <a href="http://threefold.io/" target="_blank">ThreeFold</a>
-            , a conscious tech company that saw the need for a new peer-to-peer
-            internet to empower equality (access), autonomy (data ownership),
-            and sustainability (less energy) – and acted on it.
+            and we have built a bridge to a new peer-to-peer internet. Internet
+            that empowers equality, autonomy and sustainability.
           </p>
           <p class="home_tf">
-            ThreeFold built an open source technology, a
+            As we launch the
             <a href="https://threefold.io/capacity.html" target="_blank">
               Grid
             </a>
-            of decentralized storage and compute capacity. It created a digital
-            currency, a Token [links to Token page], that represents a unit of
-            reservation of that capacity and serves as a fair and trustworthy
-            medium of exchange on the new peer-to-peer internet.
+            and other experiences on top, the time has come for us to cross over
+            to the decentralized world of peer-to-peer.
           </p>
 
-          <p class="home_tf">And it started the Alliance ..</p>
+          <p class="home_tf">
+            A decentralized peer-to-peer world would be incomplete without
+            <a href="https://threefold.io/token.html" target="_blank">
+              ThreeFold Token
+            </a>
+            - a purpose driven decentralized digital currency serving as a fair
+            and trustworthy medium of exchange.
+          </p>
+
+          <p class="home_tf">
+            A decentralized peer-to-peer world would be impossible without the
+            <a
+              href="https://www.consciousinternet.org/#/projects"
+              target="_blank">
+              Conscious Internet Alliance
+            </a>
+            , an ecosystem of beautifully aligned projects co-creating it.
+          </p>
+
+          <p class="home_tf">
+            With all the underlying elements, together we are co-creating the
+            new peer-to-peer internet, empowering a better future for humanity
+            and our planet.
+          </p>
           <p class="home_tf text-center">
             If not
             <span class="home_span">us</span>
@@ -58,11 +72,11 @@
             <span class="home_span">now</span>
             , when?
           </p>
-          <!-- <div class="text-center link_wrapper my-4">
+          <div class="text-center link_wrapper my-4">
             <a href="#/projects" class="button large text-uppercase">
               see projects
             </a>
-          </div> -->
+          </div>
         </div>
       </div>
     </article>
