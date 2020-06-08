4788347ddb2f4f9b9baf98678b86342041bf7296
hamdy <hamdy.a.farag@gmail.com>
2020-06-08 13:50:36 +0200

    fix ranking range

--- src/walker.cr
+++ src/walker.cr
@@ -98,7 +98,16 @@ def _walk(path : String = CURR_PATH)
                       end
 
                       if info.has_key?("rank")
-                        item.as(Project).info.rank =  info["rank"].as(Int64)
+                        rank : Int64 = info["rank"].as(Int64)
+                        if rank > 5_64
+                          rank = 5_64
+                        end
+
+                        if rank < 1_64
+                          rank = 1_64
+                        end
+
+                        item.as(Project).info.rank =  rank
                       end
 
                       if info.has_key?("name")
