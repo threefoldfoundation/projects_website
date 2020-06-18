9361e300a7edbc0f155cf724178352ea18404ece
samaradel <samar@incubaid.com>
2020-06-18 15:48:34 +0200

    Add pswd input

--- src/frontend/src/routes/ComingSoon.svelte
+++ src/frontend/src/routes/ComingSoon.svelte
@@ -29,14 +29,34 @@
   }
 
   .coming_soon {
-    margin-top: 15rem;
+    margin-top: 2rem;
     color: #39b1a4;
-    font-size: 2rem;
+    font-size: 1.5rem;
     line-height: 1;
   }
 
   h1 {
     font-size: 2.5rem;
+  }
+
+  input {
+    margin-top: 10px;
+  }
+
+  .mail {
+    color: #227b5c;
+  }
+
+  .form-text {
+    display: inline-block;
+  }
+
+  small {
+    line-height: 1.1;
+  }
+
+  button {
+    width: 120px !important;
   }
 </style>
 
@@ -58,18 +78,30 @@
 
       <div class="row">
         <div class="col-sm-4">
-          <p class="coming_soon font-weight-bold text-capitalize">
+          <p class="coming_soon text-capitalize my-5">
             A collective and open ecosystem of planet and human centric projects
             forming a new internet
           </p>
         </div>
       </div>
-
-      <div class="row">
-        <div class="col-sm-6">
-          <h1>Coming soon ..</h1>
+      <form class="form-inline mb-0">
+        <div class="form-group ml-0 mb-2 mr-2">
+          <label for="inputPassword2" class="sr-only">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="inputPassword2"
+            aria-describedby="passwordHelpBlock"
+            placeholder="Enter secret code" />
         </div>
-      </div>
+        <button type="button" class="button text-uppercase cta">Enter</button>
+      </form>
+      <small id="passwordHelpBlock" class="form-text text-muted">
+        If you don't have your secret code yet
+        <br />
+        please contact
+        <a class="mail" href="mailto:info@threefold.io">info@threefold.io</a>
+      </small>
     </div>
   </div>
 </div>
