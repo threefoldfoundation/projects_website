dd3d7e5f4362f866203898a17c80efc386d0bd38
samaradel <samar@incubaid.com>
2020-06-29 13:28:06 +0200

    All project cards is clickable #91

--- src/frontend/src/components/ProjectItem.svelte
+++ src/frontend/src/components/ProjectItem.svelte
@@ -6,40 +6,50 @@
   h3 {
     text-transform: none;
   }
+  .project-item p {
+    color: #646464;
+    font-size: 0.9em !important;
+    margin-top: 0;
+  }
+  a:hover {
+    text-decoration: none;
+  }
 </style>
 
 <div class="col-sm-12">
-  <article class="post search_result row p-3 mb-4">
-    <div class="col-sm-3">
-      <a class="image featured mb-0" href="#/projects/{item.name}">
+  <a class="proimage featured mb-0" href="#/projects/{item.name}">
+    <article class="post search_result row p-3 mb-4">
+      <div class="col-sm-3">
         <img src={item.links.image_path} alt="" class="img-thumbnail" />
-      </a>
-    </div>
-    <div class="col-sm-9">
-      <header>
-        <div class="title">
-          <h3>{item.name}</h3>
+      </div>
+      <div class="col-sm-9">
+        <header>
+          <div class="title">
+            <h3>{item.name}</h3>
 
-          <div>
-            {item.info.mission
-              .split(' ')
-              .slice(0, 20)
-              .join(' ')} .....
+            <div class="project-item">
+              <p>
+                {item.info.mission
+                  .split(' ')
+                  .slice(0, 20)
+                  .join(' ')}
+              </p>
+            </div>
           </div>
-        </div>
-      </header>
-      <footer class="ml-auto">
-        <ul class="stats">
-          {#each item.ecosystem.categories as category}
-            <li>
-              <a class="text-uppercase" href="#/projects/tags/{category}">
-                {category}
-              </a>
-            </li>
-          {/each}
-        </ul>
-      </footer>
-    </div>
+        </header>
+        <footer class="ml-auto">
+          <ul class="stats">
+            {#each item.ecosystem.categories as category}
+              <li>
+                <a class="text-uppercase" href="#/projects/tags/{category}">
+                  {category}
+                </a>
+              </li>
+            {/each}
+          </ul>
+        </footer>
+      </div>
+    </article>
+  </a>
 
-  </article>
 </div>
