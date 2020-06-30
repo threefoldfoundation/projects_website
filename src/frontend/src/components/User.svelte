b334490874f0e3af95e71e5c19609c55120bc4ec
samaradel <samar@incubaid.com>
2020-06-30 16:13:30 +0200

    Each user card is clickable #91

--- src/frontend/src/components/User.svelte
+++ src/frontend/src/components/User.svelte
@@ -6,38 +6,54 @@
   }
 </script>
 
+<style>
+  h3 {
+    text-transform: none;
+  }
+  .user-box p {
+    color: #646464;
+    font-size: 0.9em !important;
+    margin-top: 0;
+  }
+  a:hover {
+    text-decoration: none;
+  }
+</style>
+
 <div class="col-sm-12">
-  <article class="post search_result row p-3 mb-4">
-    <div class="col-sm-3">
-      <a class="image featured mb-0" href="#/people/{user.name}">
+  <a class="image featured mb-0" href="#/people/{user.name}">
+
+    <article class="post search_result row p-3 mb-4">
+      <div class="col-sm-3">
         <img src={user.links.image_path} alt="" class="img-thumbnail" />
-      </a>
-    </div>
-    <div class="col-sm-9">
-      <header>
-        <div class="title">
-          <h3>{formatUser()}</h3>
+      </div>
+      <div class="col-sm-9">
+        <header>
+          <div class="title">
+            <h3>{formatUser()}</h3>
 
-          <div>
-            {user.info.bio
-              .split(' ')
-              .slice(0, 20)
-              .join(' ')} .....
+            <div class="user-box">
+              <p>
+                {user.info.bio
+                  .split(' ')
+                  .slice(0, 20)
+                  .join(' ')}
+              </p>
+            </div>
           </div>
-        </div>
-      </header>
-      <footer class="ml-auto">
-        <ul class="stats">
-          {#each user.ecosystem.memberships as membership}
-            <li>
-              <a class="text-uppercase" href="#/people/tags/{membership}">
-                {membership}
-              </a>
-            </li>
-          {/each}
-        </ul>
-      </footer>
-    </div>
-
-  </article>
+        </header>
+        <footer class="ml-auto">
+          <ul class="stats">
+            {#each user.ecosystem.memberships as membership}
+              <li>
+                <a class="text-uppercase" href="#/people/tags/{membership}">
+                  {membership}
+                </a>
+              </li>
+            {/each}
+          </ul>
+        </footer>
+      </div>
+    </article>
+  </a>
 </div>
